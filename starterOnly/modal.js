31908597fe1fd107f43e3e2e90cd02a95e22d977
Dany XIONG <dany.xiong.dev@outlook.fr>
2024-02-17 18:44:33 +0100

    ✨ Homepage > Modal > Add validation message

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -15,46 +15,6 @@ const formData = document.querySelectorAll(".formData");
 /* Add */
 const modalClose = document.querySelector(".close")
 
-// const firstData = document.getElementById("first");
-// const firstElement = document.createElement("div");
-// firstElement.innerText = "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
-// firstElement.classList.add("error-message");
-
-// const lastData = document.getElementById("last");
-// const lastElement = document.createElement("div");
-// lastElement.innerText = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
-// lastElement.classList.add("error-message");
-
-// const emailData = document.getElementById("email");
-// const emailElement = document.createElement("div");
-// emailElement.innerText = "Veuillez rentrer une adresse mail valide.";
-// emailElement.classList.add("error-message");
-
-// const birthdateData = document.getElementById("birthdate");
-// const birthdateElement = document.createElement("div");
-// birthdateElement.innerText = "Merci de rentrer un date valide.";
-// birthdateElement.classList.add("error-message");
-
-// const quantityData = document.getElementById("quantity");
-// const quantityElement = document.createElement("div");
-// quantityElement.innerText = "Veuillez renseigner un nombre entre 0 et 99.";
-// quantityElement.classList.add("error-message");
-
-// const errorElement = document.querySelector(".error-message");
-// errorElement.innerText = "Veuillez indiquer une ville.";
-
-// const checkbox1Data = document.getElementById("checkbox1");
-// const checkbox1Element = document.createElement("div");
-// checkbox1Element.innerText = "Veuillez accepter les conditions d'utilisation.";
-// checkbox1Element.classList.add("error-message");
-
-// firstData.parentNode.appendChild(firstElement);
-// lastData.parentNode.appendChild(lastElement);
-// emailData.parentNode.appendChild(emailElement);
-// birthdateData.parentNode.appendChild(birthdateElement);
-// quantityData.parentNode.appendChild(quantityElement);
-// checkbox1Data.parentNode.appendChild(checkbox1Element);
-
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
@@ -72,7 +32,6 @@ function launchModal() {
 /* Add */
 function closeModal() {
   modalbg.style.display = "none";
-  console.log("La fenêtre/modal est fermée");
 }
 
 function validate() {
@@ -81,7 +40,7 @@ function validate() {
   const emailData = document.getElementById("email");
   const birthdateData = document.getElementById("birthdate");
   const quantityData = document.getElementById("quantity");
-  const radiosData = document.getElementsByName('location');
+  const radiosData = document.getElementsByName("location");
   const checkbox1Data = document.getElementById("checkbox1");
   const errorElement = document.querySelector(".error-message");
 
@@ -115,7 +74,6 @@ function validate() {
     addErrorMessage(quantityData, "Veuillez renseigner un nombre.");
   };
 
-  /**/
   for (let i = 0; i < radiosData.length; i++) {
     if (radiosData[i].checked) {
       selected = true;
@@ -162,3 +120,21 @@ function isValidEmail(email) {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return emailRegex.test(email);
 }
+
+const form = document.querySelector(".btn-submit");
+const validationElement = document.querySelector(".validation-message");
+const modalForm = document.querySelector(".modal-form");
+const closeButton = document.querySelector(".btn-close");
+
+closeButton.addEventListener("click", function(event) {
+  closeModal();
+});
+
+form.addEventListener("click", function(event) {
+  event.preventDefault();
+
+  if (validate()) {
+    modalForm.style.display = "none";
+    validationElement.style.display="block";
+  }
+});
