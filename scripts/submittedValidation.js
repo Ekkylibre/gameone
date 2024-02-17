6a54eee2435b105c37df4f0048264a4621502069
Dany XIONG <dany.xiong.dev@outlook.fr>
2024-02-18 00:38:08 +0100

    ♻️ Add last modifications

--- scripts/submittedValidation.js
+++ scripts/submittedValidation.js
@@ -1,20 +1,20 @@
-  /* DOM Elements */
+/* DOM Elements */
 
 const form = document.querySelector(".btn-submit");
 const validationElement = document.querySelector(".validation-message");
-const modalForm = document.querySelector(".modal-form");
+const modalForm = document.querySelector(".modal-body");
 const closeButton = document.querySelector(".btn-close");
 
-  /* Actions */
-closeButton.addEventListener("click", function(event) {
+/* Actions */
+closeButton.addEventListener("click", function (event) {
   closeModal();
 });
 
-form.addEventListener("click", function(event) {
+form.addEventListener("click", function (event) {
   event.preventDefault();
 
   if (validate()) {
     modalForm.style.display = "none";
-    validationElement.style.display="block";
+    validationElement.style.display = "block";
   }
 });
